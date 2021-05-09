381b410985191fe9dc41aa02e1524605c475bf6c
bart.de.lepeleer@breedbeeld.cc <ronin174>
2021-05-09 10:30:19 +0200

    Spring IOC - create cowSay from Spring

--- src/main/java/be/syntra/App.java
+++ src/main/java/be/syntra/App.java
@@ -1,19 +1,31 @@
 package be.syntra;
 
 import be.syntra.cowsay.*;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.Scanner;
 
 public class App {
 
   public static void main(String[] args) {
+
+    /**
+     * In traditional programming, the flow of the business logic is determined by objects that are statically
+     * assigned to one another.
+     * With inversion of control, the flow depends on the object graph that is instantiated
+     * by the assembler and is made possible by object interactions being defined through abstractions.
+     */
+
     /**
      * Dependency Injection is a technique of creating software in which objects do not create their dependencies on itself.
      * instead objects declare dependencies that they need and it an external object or framework to provide
      * concrete dependencies to objects
      */
 
-    CowSay cowSay = new DragonCowSay();
+    ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+    CowSay cowSay = context.getBean("cowSay", CowSay.class);
+
     QuoteDao quoteDao = new QuoteDaoMysql();
     cowSay.setQuoteDao(quoteDao);
     cowSay.showQuote();
